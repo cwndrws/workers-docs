bb40c984dc47118211a345f54e60e813a5be7b8d
Victoria <bernard.victoria22@gmail.com>
2020-01-17 12:11:48 -0600

    Revise limits and pricing to be in one section

--- workers-site/redirects/newDocs.js
+++ workers-site/redirects/newDocs.js
@@ -65,4 +65,7 @@ export const newDocsMap = new Map([
   // This is a remnant of some buggy workers.cloudflare.com code,
   // we should explicitly handle this and send them to the right place
   ['/workers/docs', '/workers'],
+
+  ['/reference/storage/limits', '/about/limits#kv'],
+  ['/reference/storage/pricing', '/about/pricing#kv'],
 ])
